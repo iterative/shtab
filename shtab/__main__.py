ff97068df01586ba6d9fc6f7b8deb3737441c05e
Casper da Costa-Luis <casper.dcl@physics.org>
2020-06-14 09:40:59 +0100

    add --error-unimportable

--- shtab/__main__.py
+++ shtab/__main__.py
@@ -4,4 +4,4 @@ import sys
 from .main import main
 
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[1:]) or 0)
--- shtab/main.py
+++ shtab/main.py
@@ -1,14 +1,18 @@
 from __future__ import absolute_import
 import argparse
 from importlib import import_module
+import logging
 
 from .shtab import complete
+
+logger = logging.getLogger(__name__)
 
 
 def get_main_parser():
     parser = argparse.ArgumentParser(prog="shtab")
     parser.add_argument("parser")
     parser.add_argument("-s", "--shell", default="bash", choices=["bash", "zsh"])
+    parser.add_argument("-u", "--error-unimportable", default=False, action="store_true")
     return parser
 
 
@@ -17,7 +21,13 @@ def main(argv=None):
     args = parser.parse_args(argv)
 
     module, other_parser = args.parser.rsplit('.', 1)
-    module = import_module(module)
+    try:
+        module = import_module(module)
+    except ImportError as err:
+        if args.error_unimportable:
+            raise
+        logger.debug(str(err))
+        return
     other_parser = getattr(module, other_parser)
     if callable(other_parser):
         other_parser = other_parser()
