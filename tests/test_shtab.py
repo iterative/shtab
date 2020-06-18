e54ef7ce6983f49acdebd72d5f82318810270efb
Casper da Costa-Luis <casper.dcl@physics.org>
2020-06-18 01:47:23 +0100

    tests: fix isort

--- tests/test_shtab.py
+++ tests/test_shtab.py
@@ -5,12 +5,41 @@ Currently runnable via nosetests, e.g.:
     shtab$ nose tests -d -v
 """
 import logging
+import subprocess
 
 import pytest
 
 import shtab
 from shtab.main import get_main_parser
-from utils import bash_compgen, bash_run
+
+
+def bash_run(init="", test="1", failure_message=""):
+    """Equivalent to `bash -c '{init}; [[ {test} ]]'`."""
+    init = init + "\n" if init else ""
+    proc = subprocess.Popen(
+        ["bash", "-c", "{init}[[ {test} ]]".format(init=init, test=test)]
+    )
+    stdout, stderr = proc.communicate()
+    assert (
+        0 == proc.wait()
+    ), """\
+{}
+{}
+=== stdout ===
+{}=== stderr ===
+{}""".format(
+        failure_message, test, stdout or "", stderr or ""
+    )
+
+
+def bash_compgen(compgen_cmd, word, expected_completions, init="", msg=""):
+    bash_run(
+        init,
+        '"$(compgen {} -- {} |xargs)" = "{}"'.format(
+            compgen_cmd, word, expected_completions
+        ),
+        msg,
+    )
 
 
 def test_bash():
