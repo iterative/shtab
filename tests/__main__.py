8bcf38e4cb3b2e35f9180ae02a40cb3a9fd3681c
Casper da Costa-Luis <casper.dcl@physics.org>
2020-06-19 19:36:26 +0100

    add codecov coverage reports

--- tests/__main__.py
+++ tests/__main__.py
@@ -5,7 +5,7 @@ from subprocess import check_call
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 check_call(
     (
-        "py.test -v --log-level=debug --cov=shtab"
+        "py.test -v --log-level=debug --cov=shtab --cov-report=xml"
         " {params} --durations=0".format(params=" ".join(sys.argv[1:]))
     ),
     shell=True,
