dacf521e2becf549635e5ade8e2d7df7b960947e
Casper da Costa-Luis <casper.dcl@physics.org>
2020-08-12 11:46:21 +0100

    tests: report coverage

--- tests/__main__.py
+++ tests/__main__.py
@@ -5,8 +5,9 @@ from subprocess import check_call
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 check_call(
     (
-        "py.test -v --log-level=debug --cov=shtab --cov-report=xml"
-        " {params} --durations=0".format(params=" ".join(sys.argv[1:]))
+        "py.test -v --log-level=debug --cov=shtab"
+        " --cov-report=xml --cov-report=term-missing --durations=0"
+        + (" ".join([""] + sys.argv[1:]))
     ),
     shell=True,
     env=dict(os.environ, PYTHONPATH=REPO_ROOT),
