14ff51ddeca8823364b91ae00c0a6ef89a6234b8
Casper da Costa-Luis <casper.dcl@physics.org>
2021-09-10 21:45:07 +0100

    pydoc-markdown

---
+++ docs/pydoc_markdown_shtab.py
@@ -0,0 +1,14 @@
+import re
+
+from pydoc_markdown.contrib.processors.pydocmd import PydocmdProcessor
+
+
+class ShtabProcessor(PydocmdProcessor):
+    def _process(self, node):
+        if not getattr(node, "docstring", None):
+            return super()._process(node)
+        # convert parameter lists to markdown list
+        node.docstring = re.sub(
+            r"^(\w+)\s{2,}(:.*?)$", r"* __\1__*\2*  ", node.docstring, flags=re.M
+        )
+        return super()._process(node)
