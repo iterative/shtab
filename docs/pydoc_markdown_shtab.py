654589402927bbe96aa9acfce89e58926c9a9f5d
Daniele Trifirò <daniele@iterative.ai>
2022-04-08 15:53:29 +0200

    fix re.sub usage for docstring processing

--- docs/pydoc_markdown_shtab.py
+++ docs/pydoc_markdown_shtab.py
@@ -8,6 +8,10 @@ class ShtabProcessor(PydocmdProcessor):
         if not getattr(node, "docstring", None):
             return super()._process(node)
         # convert parameter lists to markdown list
-        node.docstring = re.sub(r"^(\w+)\s{2,}(:.*?)$", r"* __\1__*\2*  ", node.docstring,
-                                flags=re.M)
+        node.docstring.content = re.sub(
+            r"^(\w+)\s{2,}(:.*?)$",
+            r"* __\1__*\2*  ",
+            node.docstring.content,
+            flags=re.M,
+        )
         return super()._process(node)
