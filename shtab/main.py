66354d27f58976641b1b4ebfc3ba65ffab0748e1
Peter Newman <peternewman@users.noreply.github.com>
2022-08-29 12:16:02 +0100

    Fix another typo

--- shtab/main.py
+++ shtab/main.py
@@ -13,7 +13,7 @@ log = logging.getLogger(__name__)
 
 def get_main_parser():
     parser = argparse.ArgumentParser(prog="shtab")
-    parser.add_argument("parser", help="importable parser (or fuction returning parser)")
+    parser.add_argument("parser", help="importable parser (or function returning parser)")
     parser.add_argument("--version", action="version", version="%(prog)s " + __version__)
     parser.add_argument("-s", "--shell", default=SUPPORTED_SHELLS[0], choices=SUPPORTED_SHELLS)
     parser.add_argument("--prefix", help="prepended to generated functions to avoid clashes")
