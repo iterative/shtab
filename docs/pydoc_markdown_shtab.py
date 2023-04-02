743dd7a3dbd85d4832cfcd8c44c0842422a46c54
Casper da Costa-Luis <casper.dcl@physics.org>
2023-04-02 02:52:41 +0100

    docs: fix hyperlinks

--- docs/pydoc_markdown_shtab.py
+++ docs/pydoc_markdown_shtab.py
@@ -8,10 +8,10 @@ class ShtabProcessor(PydocmdProcessor):
         if not getattr(node, "docstring", None):
             return super()._process(node)
         # convert parameter lists to markdown list
-        node.docstring.content = re.sub(
-            r"^(\w+)(:.*?)$",
-            r"* __\1__\2",
-            node.docstring.content,
-            flags=re.M,
-        )
+        node.docstring.content = re.sub(r"^(\w+)(:.*?)$", r"* __\1__\2", node.docstring.content,
+                                        flags=re.M)
+        # fix code cross-references
+        node.docstring.content = re.sub(r"<../(\S+)>",
+                                        r"[\1](https://github.com/iterative/shtab/tree/main/\1)",
+                                        node.docstring.content, flags=re.M)
         return super()._process(node)
