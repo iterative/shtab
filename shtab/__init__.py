d8590c06feb35107c4d4d72b804075c63185cf6f
Philipp Hahn <p.hahn@avm.de>
2025-10-10 15:06:46 +0200

    shellcheck: Use mapfile to preserve blanks
    https://www.shellcheck.net/wiki/SC2207
    
    Signed-off-by: Philipp Hahn <p.hahn@avm.de>

--- shtab/__init__.py
+++ shtab/__init__.py
@@ -436,17 +436,16 @@ ${root_prefix}() {
 
   if [[ "$pos_only" = 0 && "${completing_word}" == -* ]]; then
     # optional argument started: use option strings
-    COMPREPLY=( $(compgen -W "${current_option_strings[*]}" -- "${completing_word}") )
+    mapfile -t COMPREPLY < <(compgen -W "${current_option_strings[*]}" -- "${completing_word}")
   elif [[ "${previous_word}" =~ ^[0-9\\&]*[\\<\\>]\\>?$ ]]; then
     # handle redirection operators
-    COMPREPLY=( $(compgen -f -- "${completing_word}") )
+    mapfile -t COMPREPLY < <(compgen -f -- "${completing_word}")
   else
     # use choices & compgen
-    local IFS=$'\\n' # items may contain spaces, so delimit using newline
-    COMPREPLY=( $([ -n "${current_action_compgen}" ] &&
-                  "${current_action_compgen}" "${completing_word}") )
-    unset IFS
-    COMPREPLY+=( $(compgen -W "${current_action_choices[*]}" -- "${completing_word}") )
+    [ -n "${current_action_compgen}" ] &&
+      mapfile -t COMPREPLY < <("${current_action_compgen}" "${completing_word}")
+    mapfile -t -O "${#COMPREPLY[@]}" COMPREPLY < <(
+      compgen -W "${current_action_choices[*]}" -- "${completing_word}")
   fi
 
   return 0
